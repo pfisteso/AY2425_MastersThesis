3e7c3e3389c21d3fad26df1796d66bce8ac809a5
pfisteso <sophie.pfister@outlook.com>
2025-04-08 12:49:57 +0200

    adding jupyter notebook for report

---
+++ experiments/report_utils/load_data.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+
+def load_latency_data(throughput:str = '8Mbps', pipeline:str = 'conversion', max_frame: int = 525) -> pd.DataFrame:
+    filepath = os.path.join('./data', throughput, pipeline, 'latency.csv')
+    assert os.path.exists(filepath)
+    res = pd.read_csv(filepath)
+    res = res.loc[res['frame_nr'] > 0]
+    res = res.loc[res['frame_nr'] <= max_frame]
+    return res
